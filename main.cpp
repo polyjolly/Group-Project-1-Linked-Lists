158e704e652600eb2b7df2430f48dfb531ea07bc
John Greim <33739140+polyjolly@users.noreply.github.com>
2019-05-21 16:58:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 
 int DisplayMenu()
 {
-
+	int UserChoice;
+	
+	//PUT MENU OPTIONS HERE
+	
+	return UserChoice;
 }
 
 int main(int argc, char *argv[]) 
 {
 	
-  
+  	//SWITCH CASES GO HERE
   
 	system("PAUSE");
 	return 0;
