9879ed84fccbd0504a143dcb62132196621aaf63
John Greim <33739140+polyjolly@users.noreply.github.com>
2019-05-21 16:53:02 -0400

    Create Client.h

---
+++ Client.h
@@ -0,0 +1,18 @@
+//Client.h
+#include <string>
+#include<iostream>
+using namespace std;
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+class Client
+{
+	private:
+
+	public:	
+
+		
+};
+
+#endif //CLIENT_H
